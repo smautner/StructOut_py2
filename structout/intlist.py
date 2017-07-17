468052736a71840fb01a952ad07c881b1df1433a
S Mautner <mautner@cs.uni-freiburg.de>
2017-07-17 18:11:12 +0200

    hayaku!

--- structout/intlist.py
+++ structout/intlist.py
@@ -53,17 +53,17 @@ def decorate_number(num):
 ##   COMPRESS RANGES OF NUMBERS
 ########################
 
-def resize_number_array(values, desired_length):
+def resize_number_array(values, desired_length,chunk_operation=max):
     length=len(values)
     size= float(length)/desired_length
-    values = [  max(values[ int(i*size): int(math.ceil((i+1)*size))   ] )  for i in xrange(desired_length)]
+    values = [  chunk_operation(values[ int(i*size): int(math.ceil((i+1)*size))   ] )  for i in xrange(desired_length)]
     return values
 
 
 def access_region(d,start,end):
     return [ v for pos,v in d.items() if start<=pos<=end  ]
 
-def resize_number_dict(posdict, desired_length):
+def resize_number_dict(posdict, desired_length,chunk_operation=max):
     '''
     :param posdict:  {pos:NUMBER, etc}
     :param desired_length:
@@ -73,7 +73,7 @@ def resize_number_dict(posdict, desired_length):
     maxx =max(posdict)
     length= maxx-minn
     size= float(length)/desired_length
-    posdict = [max(access_region(posdict, int(i*size)+minn, int(math.ceil(i+1)*size+minn))) for i in xrange(desired_length)]
+    posdict = [chunk_operation([0]+access_region(posdict, int(i*size)+minn, int(math.ceil(i+1)*size+minn))) for i in xrange(desired_length)]
     return posdict
 
 
@@ -82,23 +82,23 @@ def resize_number_dict(posdict, desired_length):
 ###################
 # COMBINE RESIZER WITH SINGLE NUMBER TRANSFORMER
 ###################
-def numberdict_to_str(ndict, dlength):
-    ret = map(decorate_number, resize_number_dict(ndict, desired_length=dlength))
+def numberdict_to_str(ndict, dlength,chunk_operation=max):
+    ret = map(decorate_number, resize_number_dict(ndict, desired_length=dlength,chunk_operation=chunk_operation))
     return ''.join(ret)
 
-def numberlist_to_str(pdict, dlength):
-    ret = map(decorate_number, resize_number_array(pdict, desired_length=dlength))
+def numberlist_to_str(pdict, dlength,chunk_operation=max):
+    ret = map(decorate_number, resize_number_array(pdict, desired_length=dlength,chunk_operation=chunk_operation))
     return ''.join(ret)
 
 
 ###########
 #print stuff
 ###########
-def dprint(posdict,length=80):
-    print numberdict_to_str(posdict,length)
+def dprint(posdict,length=80, chunk_operation=max):
+    print numberdict_to_str(posdict,length, chunk_operation=chunk_operation)
 
-def lprint(posdict,length=80):
-    print numberlist_to_str(posdict,length)
+def lprint(posdict,length=80, chunk_operation=max):
+    print numberlist_to_str(posdict,length, chunk_operation=chunk_operation)
 
 if __name__ == "__main__":
     lprint(range(1000))
